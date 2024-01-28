e689c1d9f42b0ce2680671bd38d79b8382551df3
Mitch Downey <mitchldowney@gmail.com>
2024-01-28 17:51:24 -0600

    WIP - backup database script

---
+++ scripts/backup-db.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "Starting backup function"
+
+CONTAINER_NAME='paintsol_db'
+SOURCE_DIR="$(docker volume inspect --format '{{ .Mountpoint }}' docker_db_backup)"
+DAYS_TO_KEEP=7
+
+# daily backups
+# dump db
+
+docker exec -it ${CONTAINER_NAME} bash -c 'DATE="$(date +'%Y-%m-%d')"; pg_dump -Fp -U "user" "db" | zstd -9 --rsyncable > "/mnt/${DATE}-db.sql.zst.in_progress" && mv "/mnt/${DATE}-db.sql.zst.in_progress" "/mnt/${DATE}-db.sql.zst"'
+
+# upload to cloud storage
+
+# take contents of source directory and upload to cloud
+
+# cleanup
+echo "Find and remove old backups, before creating new one"
+echo "The following will be removed:"
+find "${SOURCE_DIR}" -maxdepth 1 -mtime +${DAYS_TO_KEEP} -name "*-daily" -print -exec rm -rfv {} \;
